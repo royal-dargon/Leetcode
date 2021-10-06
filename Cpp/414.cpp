49029346e9f3bb224e57ee9b6cfcf2a638938677
royal-dargon <2312749625@qq.com>
2021-10-06 21:58:10 +0800

    add:add a file about cpp

---
+++ Cpp/414.cpp
@@ -0,0 +1,60 @@
+// LeetCode 414题 寻找一串数组中的第三大数
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+/*
+int maxbit(vector<int>& nums,int n)
+{
+    int i;
+    int max = 0;
+    for(i = 0;i < n;i ++) {
+        if(nums[i] > max)
+        {
+            max = nums[i];
+        }
+    }
+    while(max)
+}
+*/
+
+class Solution {
+public:
+    int thirdMax(vector<int>& nums) {
+        int count = 1;
+        int i;
+        int lenth = nums.size();
+        sort(nums.begin(),nums.end());
+        if(lenth < 3){
+            return nums[lenth-1];
+        }
+        for(i = lenth-2;i >= 0;i --)
+        {
+            if(nums[i] != nums[i+1])
+            {
+                count ++;
+            }
+            if(count == 3)
+            {
+                break;
+            }
+        }
+        if(count < 3)
+        {
+            return nums[lenth-1];
+        }
+        return nums[i];
+    }
+};
+
+int main()
+{
+    Solution tmp;
+    vector<int> nums;
+    nums.push_back(3);
+    nums.push_back(2);
+    nums.push_back(2);
+    int res = tmp.thirdMax(nums);
+    cout<<res;
+    return 0;
+}
