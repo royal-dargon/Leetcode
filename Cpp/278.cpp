68926bb16e967ba0642517deffd1944951cff2c2
royal-dargon <2312749625@qq.com>
2021-10-27 16:27:42 +0800

    add:add a func

---
+++ Cpp/278.cpp
@@ -0,0 +1,23 @@
+// The API isBadVersion is defined for you.
+// bool isBadVersion(int version);
+
+class Solution {
+public:
+    int firstBadVersion(int n) {
+        long int flagl = 1;
+        long int flagr = n;
+        if(n == 1) {
+            return 1;
+        }
+        while(flagl <= flagr) {
+            long int mid = flagl + flagr;
+            mid = mid / 2;
+            if(isBadVersion(mid) == true) {
+                flagr = mid - 1;
+            } else {
+                flagl = mid + 1;
+            }
+        }
+        return flagl;
+    }
+};
