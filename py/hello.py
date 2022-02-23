a3f425bf91b6946e53bee1083136959d6262b173
royal-dargon <2312749625@qq.com>
2022-02-23 21:44:02 +0800

    add:add some code about py

---
+++ py/hello.py
@@ -0,0 +1,13 @@
+print("hello, world!")
+if True:
+    print("answer")
+    print("true")
+str = "你好啊，世界！"
+# 截取下标的方式是头下标：尾下标：步长
+print(str[0:5:2])
+# print是默认换行的，但是这种情况下是不会自动换行的
+print("hello!")
+print("world!")
+print("Hello,", end=" ")
+print("world!")
+# python共有六种数据类型，数字，列表，字符串，元组，集合，字典
