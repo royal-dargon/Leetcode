97c9c94c8256b4150a3752152d709a4d65b55d7d
royal-dargon <2312749625@qq.com>
2022-03-10 00:07:23 +0800

    add:add a new way about this question

--- py/22.py
+++ py/22.py
@@ -10,11 +10,22 @@ class Solution:
             for j in range(len(i) + 2):
                 res.add(i[0:j] + '()' + i[j:])
         return list(res)
+    
+
+    def generaterParenthesis2(self, n:int):
+        if n == 0:
+            return ['']
+        ans = []
+        for c in range(n):
+            for right in self.generaterParenthesis2(c):
+                for left in self.generaterParenthesis2(n-c-1):
+                    ans.append('({}){}'.format(left,right))
+        return ans
 
 
 def main():
     n = int(input())
-    res = Solution().generateParenthesis(n)
+    res = Solution().generaterParenthesis2(n)
     print(res)
 
 
