14e8e19a54f1c267bc6ffd685d1aaa970feb8845
royal-dargon <2312749625@qq.com>
2022-03-15 22:21:54 +0800

    add:add some code

--- practice_py/PTA/2.1.py
+++ practice_py/PTA/2.1.py
@@ -1,1 +1,9 @@
 # 这是pta的第五题计算分段函数
+from time import sleep
+
+
+x = float(input("请输入实数x:"))
+if x == 0:
+    print(f"f(0.0) = 0.0")
+else:
+    print(f"f({x:.1f}) = {1/x:.1f}")
---
+++ practice_py/蓝桥杯py训练/algorithm training/4.py
@@ -0,0 +1,5 @@
+# 算法训练的节点选择，有一棵n个节点的树，树上每个节点都有一个正整数权值
+n = int(input())
+l = list(map(int,input().split()))
+l.insert(0,0)
+print(l)
