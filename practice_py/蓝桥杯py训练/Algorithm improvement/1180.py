f04a342e4b2b8ec2b1912a676cc0df18efef722c
royal-dargon <2312749625@qq.com>
2022-04-08 17:23:42 +0800

    fix:fix a bug about this question

--- practice_py/蓝桥杯py训练/Algorithm improvement/1180.py
+++ practice_py/蓝桥杯py训练/Algorithm improvement/1180.py
@@ -8,14 +8,17 @@ for i in range(n):
     highs.append(k)
 # 下面我们希望设置两个dp，分别为dp1,dp2
 # dp1 表示的状态是目前是可以跳的， dp2 表示的状态是目前不可以跳的
-dp1 = [0 for _ in range(n)]
-dp2 = [0 for _ in range(n)]
-dp1[0] = highs[0]
-dp2[0] = 0
-dp1[1] = dp2[0] + highs[1]
-dp2[0] = 0
-for i in range(2, n):
-    dp1[i] = min(dp2[i-1] + highs[i], dp1[i-1] + highs[i])
-    dp2[i] = min(dp1[i-1], dp1[i-2])
+if n > 1:
+    dp1 = [0 for _ in range(n)]
+    dp2 = [0 for _ in range(n)]
+    dp1[0] = highs[0]
+    dp2[0] = 0
+    dp1[1] = dp2[0] + highs[1]
+    dp2[0] = 0
+    for i in range(2, n):
+        dp1[i] = min(dp2[i-1] + highs[i], dp1[i-1] + highs[i])
+        dp2[i] = min(dp1[i-1], dp1[i-2])
 
-print(min(dp1[n-1], dp2[n-1]))
+    print(min(dp1[n-1], dp2[n-1]))
+else:
+    print("0")
